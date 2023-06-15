d222974ec79a8ae5372e933b131449d2ffb087ed
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-15 10:56:48 -0300

    feat: basic structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
-import './globals.css'
+import '../app/styles/globals.css'
+import '../app/styles/colors.css'
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
