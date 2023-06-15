b65c3353de89f382ab7f6c3650846326f0cec0d5
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-15 20:04:43 -0300

    feat: fetch advice

--- src/app/features/advice/api/getAdvice.ts
+++ src/app/features/advice/api/getAdvice.ts
@@ -1,1 +1,10 @@
-import { axios } from "../axios";
+import axios from "axios";
+
+export const getAdvice = async () => {
+  try {
+    const { data } = await axios.get("https://api.adviceslip.com/advice");
+    return data.slip;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/app/features/advice/components/AdviceCard.tsx
+++ src/app/features/advice/components/AdviceCard.tsx
@@ -1,17 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import s from "./AdviceCard.module.scss";
+import { getAdvice } from "../api/getAdvice";
+import axios from "axios";
 
 export const AdviceCard = () => {
-  
+  const [advice, setAdvice] = useState({ id: 0, text: "" });
 
+  async function fetchAdvice() {
+    const { id, advice } = await getAdvice();
+    setAdvice({ id: id, text: advice });
+  }
 
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const handleClick = () => {
+    fetchAdvice();
+  };
 
   return (
     <div className={s["advice-card"]}>
-      <h3>Advice</h3>
-      <p>
-        Adice, advice, advice, advice, advice, advice, advice, advice, advice,
-        advice, advice, advice, advice, advice, advice, advice, advice, advice.
-      </p>
+      {/* TODO: There is a better way to handle Loading */}
+      {advice.text ? (
+        <>
+          <h3>Advice #{advice.id}</h3>
+          <p>{advice.text}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+
       <div>
         <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
           <g fill="none" fill-rule="evenodd">
@@ -24,7 +45,7 @@ export const AdviceCard = () => {
         </svg>
       </div>
       <div>
-        <button className={s.btn}>
+        <button className={s.btn} onClick={handleClick}>
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
