d6d4680adb470f8e0fc62124cd84f30feb9c3883
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-16 10:02:11 -0300

    feat: last

--- src/app/features/advice/components/AdviceCard.tsx
+++ src/app/features/advice/components/AdviceCard.tsx
@@ -1,37 +1,42 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import s from "./AdviceCard.module.scss";
 import { getAdvice } from "../api/getAdvice";
 import { AdviceCardDivider } from "./AdviceCardDivider";
 
-
 export const AdviceCard = () => {
   const [advice, setAdvice] = useState({
     id: 0,
-    text: "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+    text: "",
   });
+  const [loading, setLoading] = useState(false);
 
-  // async function fetchAdvice() {
-  //   const { id, advice } = await getAdvice();
-  //   setAdvice({ id: id, text: advice });
-  // }
+  async function fetchAdvice() {
+    const { id, advice } = await getAdvice();
+    setAdvice({ id: id, text: advice });
+    setLoading(false);
+  }
 
-  // useEffect(() => {
-  //   fetchAdvice();
-  // }, []);
+  const fetchAndLoad = useCallback(() => {
+    setLoading(true);
+    fetchAdvice();
+  }, []);
 
   const handleClick = () => {
-    // fetchAdvice();
+    fetchAndLoad();
   };
+
+  useEffect(() => {
+    fetchAndLoad();
+  }, [fetchAndLoad]);
 
   return (
     <div className={s["advice-card"]}>
       {/* TODO: There is a better way to handle Loading */}
-      {advice.text ? (
+      {!loading ? (
         <>
           <h3>Advice #{advice.id}</h3>
-          {/* <p className={s.quote}>{`"${advice.text}"`}</p> */}
           <div className={s.quote}>
             <p>{`${advice.text}`}</p>
           </div>
@@ -43,7 +48,8 @@ export const AdviceCard = () => {
       <AdviceCardDivider />
 
       <div>
-        <button className={s.btn} onClick={handleClick}>
+        <button className={s.btn} onClick={() => handleClick()}>
+          {/* TODO: There is a better way to organize svg as function in files */}
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
