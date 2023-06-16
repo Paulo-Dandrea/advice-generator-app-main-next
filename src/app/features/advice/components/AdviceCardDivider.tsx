8fcc88430d37216aa0142515bdcd8aedb0cfdcd0
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-16 10:43:30 -0300

    refactor: separate useScreenSize

--- src/app/features/advice/components/AdviceCardDivider.tsx
+++ src/app/features/advice/components/AdviceCardDivider.tsx
@@ -1,53 +1,49 @@
-import { useEffect, useState } from "react";
 import s from "./AdviceCard.module.scss";
+import { useScreenSize } from "../../hooks";
 
-const SmallDivider = () => (
-  <div className={s["svg-container"]}>
-    <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fill-rule="evenodd">
-        <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
-        <g transform="translate(138)" fill="#CEE3E9">
-          <rect width="6" height="16" rx="3" />
-          <rect x="14" width="6" height="16" rx="3" />
-        </g>
-      </g>
-    </svg>
-  </div>
-);
+type Size = "small" | "big";
 
-const BigDivider = () => (
-  <div className={s["svg-container"]}>
-    <svg
-      viewBox="0 0 444 16"
-      width="444"
-      height="16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g fill="none" fill-rule="evenodd">
-        <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
-        <g transform="translate(212)" fill="#CEE3E9">
-          <rect width="6" height="16" rx="3" />
-          <rect x="14" width="6" height="16" rx="3" />
+interface Prop {
+  size: Size;
+}
+
+const sizes = {
+  small: {
+    width: "295",
+    pathD: "M0 8h122v1H0zM173 8h122v1H173z",
+    gTransform: "translate(138)",
+  },
+  big: {
+    width: "444",
+    pathD: "M0 8h196v1H0zM248 8h196v1H248z",
+    gTransform: "translate(212)",
+  },
+};
+
+const Divider = ({ size }: Prop) => {
+  const width = sizes[size].width;
+
+  return (
+    <div className={s["svg-container"]}>
+      <svg
+        viewBox={`0 0 ${width} 16`}
+        width={width}
+        height="16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill="none" fill-rule="evenodd">
+          <path fill="#4F5D74" d={sizes[size].pathD} />
+          <g transform={sizes[size].gTransform} fill="#CEE3E9">
+            <rect width="6" height="16" rx="3" />
+            <rect x="14" width="6" height="16" rx="3" />
+          </g>
         </g>
-      </g>
-    </svg>
-  </div>
-);
+      </svg>
+    </div>
+  );
+};
+
 
 export function AdviceCardDivider() {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 1400);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 1400);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return <div>{isSmallScreen ? <SmallDivider /> : <BigDivider />}</div>;
+  return useScreenSize() ? <Divider size="small" /> : <Divider size="big" />;
 }
