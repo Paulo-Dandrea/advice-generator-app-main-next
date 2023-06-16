ffdb8693e6b09b75edbd9aca9a06b60a08e81b05
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-16 09:37:18 -0300

    feat: svg divider

--- src/app/features/advice/components/AdviceCardDivider.tsx
+++ src/app/features/advice/components/AdviceCardDivider.tsx
@@ -2,35 +2,44 @@ import { useEffect, useState } from "react";
 import s from "./AdviceCard.module.scss";
 
 const SmallDivider = () => (
-  <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" fill-rule="evenodd">
-      <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
-      <g transform="translate(138)" fill="#CEE3E9">
-        <rect width="6" height="16" rx="3" />
-        <rect x="14" width="6" height="16" rx="3" />
+  <div className={s["svg-container"]}>
+    <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fill-rule="evenodd">
+        <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+        <g transform="translate(138)" fill="#CEE3E9">
+          <rect width="6" height="16" rx="3" />
+          <rect x="14" width="6" height="16" rx="3" />
+        </g>
       </g>
-    </g>
-  </svg>
+    </svg>
+  </div>
 );
 
 const BigDivider = () => (
-  <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" fill-rule="evenodd">
-      <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
-      <g transform="translate(212)" fill="#CEE3E9">
-        <rect width="6" height="16" rx="3" />
-        <rect x="14" width="6" height="16" rx="3" />
+  <div className={s["svg-container"]}>
+    <svg
+      viewBox="0 0 444 16"
+      width="444"
+      height="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g fill="none" fill-rule="evenodd">
+        <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+        <g transform="translate(212)" fill="#CEE3E9">
+          <rect width="6" height="16" rx="3" />
+          <rect x="14" width="6" height="16" rx="3" />
+        </g>
       </g>
-    </g>
-  </svg>
+    </svg>
+  </div>
 );
 
 export function AdviceCardDivider() {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 1400);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 768);
+      setIsSmallScreen(window.innerWidth <= 1400);
     };
 
     window.addEventListener("resize", handleResize);
